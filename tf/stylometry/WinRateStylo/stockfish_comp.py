86315b8490c45c9e8be7ea17c2a730311b8b406a
kjljixx <kjljixx@gmail.com>
2026-02-26 15:27:40 -0500

    stockfish large games

--- tf/stylometry/WinRateStylo/stockfish_comp.py
+++ tf/stylometry/WinRateStylo/stockfish_comp.py
@@ -150,7 +150,7 @@ def process_pgns(
         opp_games = sample_games(opp_idx)
 
         # Skip positions where either player has fewer than 4 games
-        if len(stm_games) < 4 or len(opp_games) < 4:
+        if len(stm_games) < 20 or len(opp_games) < 20:
           continue
 
         pos_batch.append(pos_planes.flatten())
